957d130b390c01d22ea7c2d6b653df9928714364
cooker0910 <cooker98910@gmail.com>
2022-06-28 08:01:08 -0700

    update

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const { cry, mnemonic } = require('thor-devkit');
-
 const { Framework } = require('@vechain/connex-framework');
 const { Driver, SimpleNet, SimpleWallet } = require('@vechain/connex-driver')
 const { abi } = require('thor-devkit')
-const contractABI = require('./abi.json')
-const ADDRESS = "0x691767D45623bF22A0707eE40d90c59837d82857"
+
+const {Block} = require('./Block.model');
+const {contractABI} = require('./abi')
+const ADDRESS = "0x10363b2eaCc6D5C95Df190A69207dbA5Dbd13840"
 
 require('dotenv').config();
 
 const app = express();
-
 app.use(cors());
 
-// mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
-// db.once('open', () => {
-//   console.log("Connected to MongoDB")
-// })
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log("Connected to MongoDB")
+})
+
+let latestBlocknumber = 0;
 
 app.listen(process.env.PORT || 5000, async function () {
 
@@ -36,10 +38,108 @@ app.listen(process.env.PORT || 5000, async function () {
   const connex = new Framework(driver)
   const accForMP = connex.thor.account(ADDRESS)
   const findMethodABI = (abi, method) => abi[abi.findIndex(mthd => mthd.name === method)];
-  
-  console.log(accForMP)
-  const testMethod = accForMP.method(findMethodABI(contractABI, "testABI"))
-  console.log(testMethod)
+  const testMethod = accForMP.method(findMethodABI(contractABI, "burn"))
+
+  const step = 20;
+
+  const newEventsDecoder = (abiDefs) => {
+    const coders = {}
+
+    for (const def of abiDefs) {
+      if (def.type === 'event') {
+        const ev = new abi.Event(def)
+        coders[ev.signature] = ev
+      }
+    }
+
+    return {
+      decode: (output) => {
+        const ev = coders[output.topics[0]]
+        if (!!ev) {
+          output.decoded = ev.decode(output.data, output.topics)
+          output.event = ev.definition.name
+        }
+
+        return output
+      }
+    }
+
+  }
+
+  const getHead = async () => {
+    try {
+      // Get latest event's blocknumber and block id from mongodb
+      Block.find((err, result) => {
+        if (err) console.log("error", err)
+        else {
+          Object.values(result).map(function(block) {
+            console.log(block)
+            latestBlocknumber = block.blockID
+          })
+        }
+      })
+    } catch (error) {
+      console.error("GetHead Event Err: add event info", error);
+    }
+  }
+
+  latestBlocknumber = await getHead();
+
+  for (; ;) {
+    const blk = connex.thor.block()
+    let latestBlockNum;
+    await blk.get().then(block => {
+      latestBlockNum = block.number;
+    })
+    try {
+      await new Promise(async (resolve, reject) => {
+        if (latestBlockNum <= latestBlocknumber + 1) {
+          resolve();
+        }
+        try {
+
+          console.log("latestBlockNumber", latestBlocknumber);
+          console.log("latestBlockNum", latestBlockNum);
+
+          const Filter = connex.thor.filter('event', [{ "address": ADDRESS }]).range({ unit: "block", from: latestBlocknumber + 1, to: latestBlockNum });
+          let Offset = 0;
+          let events = [];
+          const decoder = newEventsDecoder(contractABI);
+          for (; ;) {
+            const newOutput = await Filter.apply(Offset, step).then(outputs => outputs.map(x => decoder.decode(x)));
+            events = [...events, ...newOutput];
+            if (newOutput.length == 0) {
+              break;
+            }
+            Offset += step
+          }
+
+          
+          for (let i = 0; i < events.length; i++) {
+            console.log("New Event: ", events[i].event)
+            if (events[i].event === "Transfer") {
+              console.log(events[i])
+              //do something for Transfer Event
+              // testMethod.transact().comment('Transfer')
+              // .request().then(result => {
+              //   console.log(result);
+              // })
+
+            }
+          }
+          resolve();
+        } catch (error) {
+          reject(error);
+        }
+      })
+    } catch (error) {
+      console.log("Event Fetching error" + error);
+    }
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 10 * 1000)
+    })
+  }
 
 
 });
